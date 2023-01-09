df5cc854fc944539cf115fa2e69a759620a8bd1b
anton rångeby <antonrangeby@MacBook-Air-som-tillhor-anton.local>
2023-01-09 11:03:07 +0100

    Fungerande typ

--- evil.js
+++ evil.js
@@ -5,31 +5,31 @@ var evilScientists = {
             name: 'Gandalf',
             age: 35,
             henchmen: 4,
-            description: 'En galen vetenskapsman som använder sin trollstav för att skapa förödelse bland byborna.'
+            description: 'Gandalf is a mad scientist that uses his staff to create chaos and destruction amongst the villages.'
         },
         {
             name: 'Dr boom',
             age: 45,
             henchmen: 2,
-            description: 'Dr boom älskar att spränga sina bomber överallt. Även om folk blir skadade!'
+            description: 'Dr Boom loves to blow things up with his bombs, even if other people get hurt!'
         },
         {
             name: 'Andre',
             age: 50,
             henchmen: 6,
-            description: 'Ingen klarar av denna mannen. Han har skapat ett serum som gör han sjukt jobbig'
+            description: 'No one can stand this man. He has created a serum that makes him incredibly irritating.'
         },
         {
             name: 'Putricide',
             age: 35,
             henchmen: 3,
-            description: 'Professor Putricide var en människa i sitt tidigare liv, men efter experiment förvandlas han till en zombie. Han spred sedan detta viruset vidare'
+            description: 'Professor Putricide was a human in his previous life, but after experiments he turns into a zombie. He then spread this virus further'
         },
         {
             name: 'Max',
             age: 40,
             henchmen: 10,
-            description: 'Max använde sina kunskaper inom virus forskning för att skapa ett virus som gör att alla som blir smittade blev till apor'
+            description: 'Max used his skills in virus research to create a virus that turns everyone infected into monkeys'
         },
     ]
 };
@@ -47,7 +47,6 @@ function scientistCards() {
                 break;
             }
             var NewPTag = document.createElement("p");
-            console.log(key);
             NewPTag.innerHTML = "".concat(key, ": ").concat(test, " ");
             scientistSection.appendChild(NewPTag);
         }
@@ -55,3 +54,41 @@ function scientistCards() {
     }
 }
 scientistCards();
+// Denna funktionen gör så att beroende på vilket monsterCard du klickar på så att du kan edita just den
+// function CardClicker() {
+//   const wichCard = document.querySelectorAll('.evilScientistsCard');
+//   for (const key of wichCard.keys()) {
+//       wichCard[key].onclick = function() {
+//           for (let [key1, key2] of Object.entries(monsterObject.monsters[key])) {
+//               if (key1 == "apperences") {
+//                   for (let k = 0; k < apperence.length; k++) {
+//                       let key1 = Editinput[k].value
+//                       key2[k] = `${apperence[k]}: ${key1}`
+//                   }
+//               }
+//               monsterInformation.innerHTML = '';
+//           }
+//           monsterObject.createMonsters()
+//       }
+//       } 
+//   }
+function testar() {
+    var currentScientist = document.querySelector(".currentScientist");
+    var wichCard = document.querySelectorAll('.evilScientistsCard');
+    var _loop_1 = function (i) {
+        wichCard[i].onclick = function () {
+            console.log(wichCard[i]);
+            for (var _i = 0, _a = Object.keys(evilScientists.scientists[i]); _i < _a.length; _i++) {
+                var key = _a[_i];
+                var test2 = evilScientists.scientists[i][key];
+                var NewPTag = document.createElement("p");
+                NewPTag.innerHTML = "".concat(key, ": ").concat(test2, " ");
+                currentScientist.appendChild(NewPTag);
+            }
+        };
+    };
+    for (var i = 0; i < wichCard.length; i++) {
+        _loop_1(i);
+    }
+}
+testar();
--- evil.ts
+++ evil.ts
@@ -7,31 +7,32 @@ const evilScientists = {
             name: 'Gandalf',
             age: 35,
             henchmen: 4,
-            description: 'En galen vetenskapsman som använder sin trollstav för att skapa förödelse bland byborna.'
+            description: 'Gandalf is a mad scientist that uses his staff to create chaos and destruction amongst the villages.'
         },
         {
             name: 'Dr boom',
             age: 45,
             henchmen: 2,
-            description: 'Dr boom älskar att spränga sina bomber överallt. Även om folk blir skadade!'
+            description: 'Dr Boom loves to blow things up with his bombs, even if other people get hurt!'
         },
         {
             name: 'Andre',
             age: 50,
             henchmen: 6,
-            description: 'Ingen klarar av denna mannen. Han har skapat ett serum som gör han sjukt jobbig'
+            description: 'No one can stand this man. He has created a serum that makes him incredibly irritating.'
+            
         },
         {
             name: 'Putricide',
             age: 35,
             henchmen: 3,
-            description: 'Professor Putricide var en människa i sitt tidigare liv, men efter experiment förvandlas han till en zombie. Han spred sedan detta viruset vidare'
+            description: 'Professor Putricide was a human in his previous life, but after experiments he turns into a zombie. He then spread this virus further'
         },
         {
             name: 'Max',
             age: 40,
             henchmen: 10,
-            description: 'Max använde sina kunskaper inom virus forskning för att skapa ett virus som gör att alla som blir smittade blev till apor'
+            description: 'Max used his skills in virus research to create a virus that turns everyone infected into monkeys'
         },
     ]
 }
@@ -65,3 +66,47 @@ function scientistCards() {
 }
 
 scientistCards()
+
+// Denna funktionen gör så att beroende på vilket monsterCard du klickar på så att du kan edita just den
+// function CardClicker() {
+//   const wichCard = document.querySelectorAll('.evilScientistsCard');
+//   for (const key of wichCard.keys()) {
+      
+//       wichCard[key].onclick = function() {
+//           for (let [key1, key2] of Object.entries(monsterObject.monsters[key])) {
+              
+//               if (key1 == "apperences") {
+//                   for (let k = 0; k < apperence.length; k++) {
+//                       let key1 = Editinput[k].value
+//                       key2[k] = `${apperence[k]}: ${key1}`
+//                   }
+//               }
+//               monsterInformation.innerHTML = '';
+//           }
+//           monsterObject.createMonsters()
+//       }
+//       } 
+//   }
+
+
+  function testar() {
+    const currentScientist = document.querySelector(".currentScientist") as HTMLElement;
+    const wichCard = document.querySelectorAll('.evilScientistsCard') as NodeList;
+      for (let i = 0; i < wichCard.length; i++) {
+        wichCard[i].onclick = () => {
+      console.log(wichCard[i])
+      for (const key of Object.keys(evilScientists.scientists[i])) {
+        let test2 = evilScientists.scientists[i][key];
+          
+      const NewPTag = document.createElement("p");
+      NewPTag.innerHTML = `${key}: ${test2} `;
+      
+      currentScientist.appendChild(NewPTag)
+      }
+  };
+}
+   
+
+  }
+  testar()
+  
